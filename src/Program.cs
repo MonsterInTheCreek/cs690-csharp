70516a744a6f1b547252d1a87cdd0e717530ea20
David Turner <76234472+MonsterInTheCreek@users.noreply.github.com>
2025-01-20 15:02:52 -0500

    Improve runtime UI of entry point

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace src.blocks
 {
@@ -6,11 +8,28 @@ namespace src.blocks
     {
         static void Main(string[] args)
         {
-            // Will improve this UI a bit with time, simple for now
+            Console.WriteLine("Files currently available for call:");
 
-            Console.WriteLine("Enter a codeblock to call: ");
+            // Use IO and regex to get all files in ./blocks dir
+            string[] files = Directory.GetFiles("./blocks/");
+            string pattern = @"\.\/blocks\/(\w+).cs";
+            Regex regex = new Regex(pattern);
+            int fileCount = files.Count();
+            for (int i = 0; i < fileCount; i++)
+            {
+                Match match = regex.Match(files[i]);
+                if (i == fileCount - 1)
+                {
+                    Console.Write(match.Groups[1] + "\n");
+                } else {
+                    Console.Write(match.Groups[1] + ", ");
+                }
+            }
+
+            Console.WriteLine("\nEnter a codeblock to call: ");
             string blockName = Console.ReadLine();
 
+            // call specific ./blocks codeblock entered
             switch (blockName.ToLower())
             {
                 case "example":
